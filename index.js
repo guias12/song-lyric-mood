e2072c93357d72fa4c66524c6cbd490534e7249e
guias12 <guilherme.p.assis@gmail.com>
2019-05-12 19:57:16 -0300

    requisicao de busca de musica esta mais eficaz

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 require('dotenv/config');
 const axios = require('axios');
+const watson = require('ibm-watson');
 
 gettingSongData();
 
 function gettingSongData(){
 	//those should be dynamic
 	let artist = "Queen";
-	let song = "Bohemian Rhapsody";
+	let song = "Radio Ga Ga";
 	axios.get(
-        `${process.env.MUSIXMATCH_API_URL}track.search?page=1&page_size=10&q_artist=${artist}&q_track=${song}&apikey=${process.env.MUSIXMATCH_API_KEY}`
+        `${process.env.MUSIXMATCH_API_URL}track.search?page=1&page_size=10&f_has_lyrics=true&q_artist=${artist}&q_track=${song}&s_artist_rating=desc&s_track_rating=desc&apikey=${process.env.MUSIXMATCH_API_KEY}`
     ).then(res => {    	
-    	let songId = res.data.message.body.track_list[0].track.track_id;
+    	let songId = res.data.message.body.track_list[0].track.track_id;    	
       	getSongLyric(songId);
     }).catch(err => console.log(err, 'Somenthing went wrong'));  	
 }
@@ -19,6 +20,8 @@ function getSongLyric(songId){
 	axios.get(
 	`${process.env.MUSIXMATCH_API_URL}track.lyrics.get?track_id=${songId}&apikey=${process.env.MUSIXMATCH_API_KEY}`
 	).then(res =>{
-		console.log(res.data, songId);
+		let songLyric = res.data.message.body.lyrics.lyrics_body, songId;
+		watsonToneAlanyzer(songLyric);
 	}).catch(err => console.log(err, 'Somenthing went wrong'));
 }
+
